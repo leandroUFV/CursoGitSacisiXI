4f112c2d4acf5f93837591beb05236823d5e7b2c
leandro guimaraes <leandroguioliveira@hotmail.com>
2019-06-13 19:57:51 -0300

    adicionando funcao de subtracao

--- projeto.cpp
+++ projeto.cpp
@@ -5,8 +5,16 @@ int soma(int a, int b){
 	return soma_num;
 }
 
+
+
+int subtracao(int a, int b){
+	int sub_num = a-b;
+	return sub_num;
+}
+
 int main (){
 	printf("soma dos numeros: %d \n", soma(5,5));
-	
+	printf("subtracao dos numeros: %d \n", subtracao(5,2));
+
 	return 0;
 }
